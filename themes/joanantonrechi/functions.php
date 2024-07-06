03547cd748d21c2deb21fcac4cf7ede0d420db6d
Sebaspa0107 <sebaspa0107@gmail.com>
2024-07-06 13:32:52 -0500

    🏗️ Creacion estructura de template

---
+++ themes/joanantonrechi/functions.php
@@ -0,0 +1,45 @@
+<?php
+
+if (!defined('VERSION')) {
+  define('VERSION', '1.0.0');
+}
+
+function joanantonrechi_setup()
+{
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_theme_support(
+    'html5',
+    array(
+      //'search-form',
+      //'comment-form',
+      //'comment-list',
+      //'gallery',
+      //'caption',
+      //'style',
+      //'script',
+    )
+  );
+
+  register_nav_menus(
+    array(
+      'menu-1' => esc_html__('Primary', 'joanantonrechi'),
+    )
+  );
+
+  //add_image_size('product_thumbnail', 310, 350, true);
+
+}
+
+add_action('after_setup_theme', 'joanantonrechi_setup');
+
+
+
+function joanantonrechi_styles_scripts()
+{
+
+  wp_enqueue_style('joanantonrechi-style', get_template_directory_uri() . '/dist/css/app.css', array('font-awesome'), VERSION);
+  wp_enqueue_script('joanantonrechi-script', get_template_directory_uri() . '/dist/js/app.js', array('jquery'), VERSION, true);
+}
+
+add_action('wp_enqueue_scripts', 'joanantonrechi_styles_scripts');
